393231c778eeadb1102b7789e9c1df0d3a6117e7
Jeyem <suariojohnrodmhar52@gmail.com>
2024-12-06 19:39:29 +0800

    Added TaskManagement

--- IIMS Final Project/TaskManagement.Designer.cs
+++ IIMS Final Project/TaskManagement.Designer.cs
@@ -28,12 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(436, 323);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "EMPLOYEE:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(439, 339);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(200, 21);
+            this.comboBox2.TabIndex = 22;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(161, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "DEADLINE:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(436, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "TASK:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(161, 276);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "SELECT PROJECT WHERE TO CREATE TASK:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(529, 390);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "DELETE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(370, 390);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "EDIT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(164, 295);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 21);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(164, 341);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 14;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(439, 295);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(164, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(475, 221);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // TaskManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "TaskManagement";
             this.Text = "TaskManagement";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- IIMS Final Project/TaskManagement.cs
+++ IIMS Final Project/TaskManagement.cs
@@ -2,19 +2,264 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace IIMS_Final_Project
 {
     public partial class TaskManagement : Form
     {
+        private string connectionString = "Server=localhost;Database=iims_finalproject;User=root;Password=jrms;";
+        private MySqlConnection conn;
+
         public TaskManagement()
         {
             InitializeComponent();
+            InitializeComponent();
+            LoadProjects();
+            LoadEmployees();
+            LoadTasks();
+
+            // Set DropDownStyle to DropDownList for both ComboBoxes
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            // Set SelectedIndex to -1 to ensure no item is selected initially
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
+
+        }
+
+        private void LoadProjects()
+        {
+            comboBox1.Items.Clear(); // Clear previous items
+            List<KeyValuePair<int, string>> projects = new List<KeyValuePair<int, string>>();
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT project_id, project_name FROM Project"; // Assuming you have a Project table
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int projectId = Convert.ToInt32(reader["project_id"]);
+                    string projectName = reader["project_name"].ToString();
+                    projects.Add(new KeyValuePair<int, string>(projectId, projectName));
+                }
+                reader.Close();
+            }
+
+            // Set the ComboBox data source
+            comboBox1.DataSource = projects;
+            comboBox1.DisplayMember = "Value";  // This will display the project name
+            comboBox1.ValueMember = "Key";     // This will hold the project ID
+        }
+
+
+        private void LoadEmployees()
+        {
+            comboBox2.Items.Clear(); // Clear previous items
+            List<KeyValuePair<int, string>> employees = new List<KeyValuePair<int, string>>();
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT employee_id, CONCAT(first_name, ' ', last_name) AS employee_name FROM Employee WHERE status = 'Active'";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int employeeId = Convert.ToInt32(reader["employee_id"]);
+                    string employeeName = reader["employee_name"].ToString();
+                    employees.Add(new KeyValuePair<int, string>(employeeId, employeeName));
+                }
+                reader.Close();
+            }
+
+            // Set the ComboBox data source
+            comboBox2.DataSource = employees;
+            comboBox2.DisplayMember = "Value";  // This will display the employee name
+            comboBox2.ValueMember = "Key";     // This will hold the employee ID
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Check if an item is selected in comboBox1 and comboBox2
+            if (comboBox1.SelectedItem == null || comboBox2.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a project and an employee.");
+                return;
+            }
+
+            // Cast the selected item to KeyValuePair<int, string> to access the Value and Key
+            int projectId = ((KeyValuePair<int, string>)comboBox1.SelectedItem).Key;
+            int assignedTo = ((KeyValuePair<int, string>)comboBox2.SelectedItem).Key;
+            string taskName = textBox1.Text;
+            DateTime dueDate = dateTimePicker1.Value;
+            string status = "Pending"; // Default status
+
+            if (string.IsNullOrEmpty(taskName) || projectId == 0 || assignedTo == 0)
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return;
+            }
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "INSERT INTO Task (task_name, assigned_to, project_id, status, due_date) " +
+                               "VALUES (@taskName, @assignedTo, @projectId, @status, @dueDate)";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@taskName", taskName);
+                cmd.Parameters.AddWithValue("@assignedTo", assignedTo);
+                cmd.Parameters.AddWithValue("@projectId", projectId);
+                cmd.Parameters.AddWithValue("@status", status);
+                cmd.Parameters.AddWithValue("@dueDate", dueDate);
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Task added successfully.");
+            LoadTasks(); // Refresh the DataGridView
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Check if a task is selected in DataGridView
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a task to edit.");
+                return;
+            }
+
+            // Get the selected task ID
+            int taskId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["task_id"].Value);
+
+            // Get the values from the form (ComboBoxes, TextBox, DateTimePicker)
+            string taskName = textBox1.Text;
+
+            // Ensure comboBox1 has a valid selection
+            if (comboBox1.SelectedValue == null)
+            {
+                MessageBox.Show("Please select a project.");
+                return;
+            }
+            int projectId = (int)comboBox1.SelectedValue; // Get the selected project ID
+
+            // Ensure comboBox2 has a valid selection
+            if (comboBox2.SelectedValue == null)
+            {
+                MessageBox.Show("Please select an employee.");
+                return;
+            }
+            int assignedTo = (int)comboBox2.SelectedValue; // Get the selected employee ID
+
+            DateTime dueDate = dateTimePicker1.Value;
+            string status = "Pending"; // Default status
+
+            // Check if all fields are filled
+            if (string.IsNullOrEmpty(taskName) || projectId == 0 || assignedTo == 0)
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return;
+            }
+
+            // Update the task in the database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "UPDATE Task SET task_name = @taskName, assigned_to = @assignedTo, project_id = @projectId, status = @status, due_date = @dueDate WHERE task_id = @taskId";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@taskName", taskName);
+                cmd.Parameters.AddWithValue("@assignedTo", assignedTo);
+                cmd.Parameters.AddWithValue("@projectId", projectId);
+                cmd.Parameters.AddWithValue("@status", status);
+                cmd.Parameters.AddWithValue("@dueDate", dueDate);
+                cmd.Parameters.AddWithValue("@taskId", taskId);
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Task updated successfully.");
+            LoadTasks(); // Refresh the DataGridView
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Check if a task is selected in DataGridView
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a task to delete.");
+                return;
+            }
+
+            // Get the selected task ID
+            int taskId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["task_id"].Value);
+
+            // Delete the task from the database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "DELETE FROM Task WHERE task_id = @taskId";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@taskId", taskId);
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Task deleted successfully.");
+            LoadTasks(); // Refresh the DataGridView
+        }
+
+        private void LoadTasks()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString)) // Use the actual connection string variable
+            {
+                conn.Open();
+                string query = "SELECT t.task_id, t.task_name, t.status, t.due_date, e.first_name, e.last_name, p.project_name " +
+                               "FROM Task t " +
+                               "JOIN Employee e ON t.assigned_to = e.employee_id " +
+                               "JOIN Project p ON t.project_id = p.project_id";
+                MySqlDataAdapter da = new MySqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                // Set DataSource
+                dataGridView1.DataSource = dt;
+
+                // Hide the task_id column (assuming task_id is the first column)
+                dataGridView1.Columns["task_id"].Visible = false;
+            }
         }
     }
 }
